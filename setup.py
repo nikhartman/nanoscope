6755cc4a8bc375b72b76c42109c97c8b3a629bf8
nikhartman <nik.hartman@gmail.com>
2015-01-17 15:21:03 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 
 setup(name = 'nanoscope',
       version = '1.0',
-      description = 'tools for handling data from Nanoscope V5 software'
+      description = 'tools for handling data from Nanoscope V5 software',
       author = 'Nik Hartman',
       author_email = 'nik.hartman@gmail.com',
       url = 'https://github.com/nikhartman/nanoscope',
-      py_modules = ['nanoscope'],
+      py_modules = ['nanoscope']
       )
